c14b193fc185b9227c3d2d0ca18d1f51b9867fa2
Fons van der Plas <fonsvdplas@gmail.com>
2017-05-14 02:39:33 +0200

    Derp

---
+++ dev/NancySelfHost1/NancySelfHost1/Modules/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace NancySelfHost1.Modules
+{
+	public class IndexModule : NancyModule
+	{
+		public IndexModule()
+		{
+			Get["/"] = parameters =>
+			{
+				return View["index"];
+			};
+			Get["/{whatever*}"] = _ => Response.AsRedirect("/");
+		}
+	}
+}
---
+++ dev/NancySelfHost1/NancySelfHost1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using Nancy;
+
+namespace NancySelfHost1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int port = 80;
+			if(args.Length > 0)
+			{
+				int.TryParse(args[0], out port);
+			}
+			var uri =
+				new Uri("http://localhost:" + port);
+			HostConfiguration hostConfigs = new HostConfiguration();
+			hostConfigs.UrlReservations.CreateAutomatically = true;
+
+			using(var host = new NancyHost(uri, new DefaultNancyBootstrapper(), hostConfigs))
+			{
+				host.Start();
+
+				Console.WriteLine("Your application is running on " + uri);
+				Console.WriteLine("Press any [Enter] to close the host.");
+				Console.ReadLine();
+			}
+		}
+	}
+}
